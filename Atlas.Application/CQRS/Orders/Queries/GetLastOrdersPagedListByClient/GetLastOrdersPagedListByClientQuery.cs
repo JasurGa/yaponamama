9e66919e85d32d766d0a6ac3769625ffb400631e
Deast <xtreme-2017@yandex.ru>
2022-04-19 19:01:30 +0500

    Last orders functionality added

---
+++ Atlas.Application/CQRS/Orders/Queries/GetLastOrdersPagedListByClient/GetLastOrdersPagedListByClientQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Atlas.Application.Models;
+using MediatR;
+
+namespace Atlas.Application.CQRS.Orders.Queries.GetLastOrdersPagedListByClient
+{
+    public class GetLastOrdersPagedListByClientQuery : IRequest<PageDto<OrderLookupDto>>
+    {
+        public Guid ClientId { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int PageIndex { get; set; }
+    }
+}
+}
