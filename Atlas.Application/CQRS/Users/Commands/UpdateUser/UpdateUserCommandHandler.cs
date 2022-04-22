98aa94d07f04b84517e67d5df7bf6fc055ce8c94
telesium <92969613+telesium@users.noreply.github.com>
2022-04-22 15:58:27 +0500

    Changed user fields

--- Atlas.Application/CQRS/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
+++ Atlas.Application/CQRS/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -16,23 +16,25 @@ namespace Atlas.Application.CQRS.Users.Commands.UpdateUser
     {
         private readonly IAtlasDbContext _dbContext;
 
-        public UpdateUserCommandHandler(IAtlasDbContext dbContext) => _dbContext = dbContext;
+        public UpdateUserCommandHandler(IAtlasDbContext dbContext) => 
+            _dbContext = dbContext;
 
         public async Task<Unit> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
-            var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+            var user = await _dbContext.Users.FirstOrDefaultAsync(x =>
+                x.Id == request.Id, cancellationToken);
 
             if (user == null)
             {
                 throw new NotFoundException(nameof(User), request.Id);
             }
 
-            user.FirstName  = request.FirstName;
-            user.LastName   = request.LastName;
-            user.Birthday   = request.Birthday;
+            user.FirstName       = request.FirstName;
+            user.LastName        = request.LastName;
+            user.Birthday        = request.Birthday;
+            user.AvatarPhotoPath = request.AvatarPhotoPath;
 
             await _dbContext.SaveChangesAsync(cancellationToken);
-
             return Unit.Value;
         }
     }
