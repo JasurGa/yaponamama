cced5f3016c21f3169d131b070510fdff163cea2
telesium <92969613+telesium@users.noreply.github.com>
2022-04-22 15:50:02 +0500

    Changed update courier fields

--- Atlas.Application/CQRS/Couriers/Commands/UpdateCourier/UpdateCourierCommand.cs
+++ Atlas.Application/CQRS/Couriers/Commands/UpdateCourier/UpdateCourierCommand.cs
@@ -6,9 +6,9 @@ namespace Atlas.Application.CQRS.Couriers.Commands.UpdateCourier
     public class UpdateCourierCommand : IRequest
     {
         public Guid Id { get; set; }
+        
+        public string PassportPhotoPath { get; set; }
 
-        public string PhoneNumber { get; set; }
-
-        public Guid VehicleId { get; set; }
+        public string DriverLicensePath { get; set; }
     }
 }
