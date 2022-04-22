6598c87c0b9265f5b1797e3b64939fd9be002a3f
telesium <92969613+telesium@users.noreply.github.com>
2022-04-22 16:05:02 +0500

    Update UpdateCourierDto.cs

--- Atlas.Presentation/Atlas.WebApi/Models/UpdateCourierDto.cs
+++ Atlas.Presentation/Atlas.WebApi/Models/UpdateCourierDto.cs
@@ -8,20 +8,20 @@ namespace Atlas.WebApi.Models
     public class UpdateCourierDto : IMapWith<UpdateCourierCommand>
     {
         public Guid Id { get; set; }
+        
+        public string PassportPhotoPath { get; set; }
 
-        public string PhoneNumber { get; set; }
-
-        public Guid? VehicleId { get; set; }
+        public string DriverLicensePath { get; set; }
 
         public void Mapping(Profile profile)
         {
-            profile.CreateMap<UpdateClientDto, UpdateCourierCommand>()
+            profile.CreateMap<UpdateCourierDto, UpdateCourierCommand>()
                 .ForMember(x => x.Id, opt =>
                     opt.MapFrom(src => src.Id))
-                .ForMember(x => x.PhoneNumber, opt =>
-                    opt.MapFrom(src => src.PhoneNumber))
-                .ForMember(x => x.VehicleId, opt =>
-                    opt.MapFrom(src => src.VehicleId));
+                .ForMember(x => x.PassportPhotoPath, opt =>
+                    opt.MapFrom(src => src.PassportPhotoPath))
+                .ForMember(x => x.DriverLicensePath, opt =>
+                    opt.MapFrom(src => src.DriverLicensePath));
         }
     }
 }
