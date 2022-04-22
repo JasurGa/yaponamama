90fabb187b52068b1754d98a11aa069896727ebc
telesium <92969613+telesium@users.noreply.github.com>
2022-04-22 16:06:12 +0500

    Update UpdateUserDto.cs

--- Atlas.Presentation/Atlas.WebApi/Models/UpdateUserDto.cs
+++ Atlas.Presentation/Atlas.WebApi/Models/UpdateUserDto.cs
@@ -14,14 +14,22 @@ namespace Atlas.WebApi.Models
         public string LastName { get; set; }
 
         public DateTime Birthday { get; set; }
+        
+        public string AvatarPhotoPath { get; set; }
 
         public void Mapping(Profile profile)
         {
             profile.CreateMap<UpdateUserDto, UpdateUserCommand>()
-                .ForMember(x => x.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(x => x.FirstName, opt => opt.MapFrom(src => src.FirstName))
-                .ForMember(x => x.LastName, opt => opt.MapFrom(src => src.LastName))
-                .ForMember(x => x.Birthday, opt => opt.MapFrom(src => src.Birthday));
+                .ForMember(x => x.Id, opt => 
+                    opt.MapFrom(src => src.Id))
+                .ForMember(x => x.FirstName, opt => 
+                    opt.MapFrom(src => src.FirstName))
+                .ForMember(x => x.LastName, opt => 
+                    opt.MapFrom(src => src.LastName))
+                .ForMember(x => x.Birthday, opt => 
+                    opt.MapFrom(src => src.Birthday))
+                .ForMember(x => x.AvatarPhotoPath, opt =>
+                    opt.MapFrom(src => src.AvatarPhotoPath));
         }
     }
 }
