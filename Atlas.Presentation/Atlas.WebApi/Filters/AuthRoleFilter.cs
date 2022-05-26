82a8ee91ce3c228783e446f725c170a7b43557ee
telesium <telesium@protonmail.com>
2022-05-26 19:34:27 +0500

    Auth filter added

---
+++ Atlas.Presentation/Atlas.WebApi/Filters/AuthRoleFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Atlas.Application.Common.Constants;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Atlas.WebApi.Filters
+{
+    public class AuthRoleFilter : ActionFilterAttribute, IAuthorizationFilter
+    {
+        public string[] RoleClaims { get; set; }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+
+            var hasAny = false;
+            foreach (var role in RoleClaims)
+            {
+                try
+                {
+                    var adminId = context.HttpContext.User.FindFirst(role).Value;
+                    hasAny = true;
+                    break;
+                }
+                catch (ArgumentNullException)
+                {
+                    continue;
+                }
+            }
+
+            if (!hasAny)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+    }
+}
