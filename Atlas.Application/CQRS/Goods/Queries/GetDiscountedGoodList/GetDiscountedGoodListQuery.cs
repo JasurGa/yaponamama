a852303ce11c95b1c0893577854088ca4cc8cc05
Deast <xtreme-2017@yandex.ru>
2022-08-13 15:04:18 +0500

    Added getting discounted goods without pagination

---
+++ Atlas.Application/CQRS/Goods/Queries/GetDiscountedGoodList/GetDiscountedGoodListQuery.cs
@@ -0,0 +1,10 @@
+﻿using Atlas.Application.CQRS.Goods.Queries.GetGoodListByCategory;
+using MediatR;
+
+namespace Atlas.Application.CQRS.Goods.Queries.GetDiscountedGoodList
+{
+    public class GetDiscountedGoodListQuery : IRequest<GoodListVm>
+    {
+
+    }
+}
