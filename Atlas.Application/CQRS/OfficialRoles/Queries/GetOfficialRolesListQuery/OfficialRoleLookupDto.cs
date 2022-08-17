b91a3a97516005d6cde64e788e0df414278b1c45
telesium <telesium@protonmail.com>
2022-08-17 11:45:10 +0500

    Official roles and payment types localization added

--- Atlas.Application/CQRS/OfficialRoles/Queries/GetOfficialRolesListQuery/OfficialRoleLookupDto.cs
+++ Atlas.Application/CQRS/OfficialRoles/Queries/GetOfficialRolesListQuery/OfficialRoleLookupDto.cs
@@ -11,13 +11,25 @@ namespace Atlas.Application.CQRS.OfficialRoles.Queries.GetOfficialRolesListQuery
 
         public string Name { get; set; }
 
+        public string NameRu { get; set; }
+
+        public string NameEn { get; set; }
+
+        public string NameUz { get; set; }
+
         public void Mapping(Profile profile)
         {
             profile.CreateMap<OfficialRole, OfficialRoleLookupDto>()
                 .ForMember(x => x.Id, opt =>
                     opt.MapFrom(x => x.Id))
                 .ForMember(x => x.Name, opt =>
-                    opt.MapFrom(x => x.Name));
+                    opt.MapFrom(x => x.Name))
+                .ForMember(x => x.NameRu, opt =>
+                    opt.MapFrom(x => x.NameRu))
+                .ForMember(x => x.NameEn, opt =>
+                    opt.MapFrom(x => x.NameEn))
+                .ForMember(x => x.NameUz, opt =>
+                    opt.MapFrom(x => x.NameUz));
         }
     }
 }
--- Atlas.Application/CQRS/PaymentTypes/Queries/GetPaymentTypeList/PaymentTypeLookupDto.cs
+++ Atlas.Application/CQRS/PaymentTypes/Queries/GetPaymentTypeList/PaymentTypeLookupDto.cs
@@ -11,13 +11,25 @@ namespace Atlas.Application.CQRS.PaymentTypes.Queries.GetPaymentTypeList
 
         public string Name { get; set; }
 
+        public string NameRu { get; set; }
+
+        public string NameEn { get; set; }
+
+        public string NameUz { get; set; }
+
         public void Mapping(Profile profile)
         {
             profile.CreateMap<PaymentType, PaymentTypeLookupDto>()
                 .ForMember(dst => dst.Id, opt =>
                     opt.MapFrom(src => src.Id))
                 .ForMember(dst => dst.Name, opt =>
-                    opt.MapFrom(src => src.Name));
+                    opt.MapFrom(src => src.Name))
+                .ForMember(dst => dst.NameRu, opt =>
+                    opt.MapFrom(src => src.NameRu))
+                .ForMember(dst => dst.NameEn, opt =>
+                    opt.MapFrom(src => src.NameEn))
+                .ForMember(dst => dst.NameUz, opt =>
+                    opt.MapFrom(src => src.NameUz));
         }
     }
 }
