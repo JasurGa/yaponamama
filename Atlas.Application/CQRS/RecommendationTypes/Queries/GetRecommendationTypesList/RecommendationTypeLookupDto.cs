a54e05f270f81b7d3bdd4a3db4253b6723d4f1ee
telesium <telesium@protonmail.com>
2022-08-17 11:54:56 +0500

    Recommendation type logicy added

--- Atlas.Application/CQRS/RecommendationTypes/Queries/GetRecommendationTypesList/RecommendationTypeLookupDto.cs
+++ Atlas.Application/CQRS/RecommendationTypes/Queries/GetRecommendationTypesList/RecommendationTypeLookupDto.cs
@@ -11,13 +11,25 @@ namespace Atlas.Application.CQRS.RecommendationTypes.Queries.GetRecommendationTy
 
         public string Name { get; set; }
 
+        public string NameRu { get; set; }
+
+        public string NameEn { get; set; }
+
+        public string NameUz { get; set; }
+
         public void Mapping(Profile profile)
         {
             profile.CreateMap<RecommendationType, RecommendationTypeLookupDto>()
                 .ForMember(dst => dst.Id, opt =>
                     opt.MapFrom(src => src.Id))
                 .ForMember(dst => dst.Name, opt =>
-                    opt.MapFrom(src => src.Name));
+                    opt.MapFrom(src => src.Name))
+                .ForMember(dst => dst.NameRu, opt =>
+                    opt.MapFrom(src => src.NameRu))
+                .ForMember(dst => dst.NameEn, opt =>
+                    opt.MapFrom(src => src.NameEn))
+                .ForMember(dst => dst.NameUz, opt =>
+                    opt.MapFrom(src => src.NameUz));
         }
     }
 }
