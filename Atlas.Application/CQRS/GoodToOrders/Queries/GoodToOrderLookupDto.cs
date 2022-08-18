691927885c984a184f89a961de8db29d6c3d6a3d
Deast <xtreme-2017@yandex.ru>
2022-08-18 16:27:43 +0500

    Added missing data to order list

---
+++ Atlas.Application/CQRS/GoodToOrders/Queries/GoodToOrderLookupDto.cs
@@ -0,0 +1,47 @@
+﻿using Atlas.Application.Common.Mappings;
+using Atlas.Domain;
+using AutoMapper;
+using System;
+
+namespace Atlas.Application.CQRS.GoodToOrders.Queries
+{
+    public class GoodToOrderLookupDto : IMapWith<GoodToOrder>
+    {
+        public Guid Id { get; set; }
+
+        public Guid GoodId { get; set; }
+
+        public string GoodName { get; set; }
+
+        public string GoodImagePath { get; set; }
+
+        public long GoodSellingPrice { get; set; }
+
+        public float GoodDiscount { get; set; }
+
+        public Guid OrderId { get; set; }
+
+        public int Count { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<GoodToOrder, GoodToOrderLookupDto>()
+                .ForMember(dest => dest.Id, opt =>
+                    opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.GoodId, opt =>
+                    opt.MapFrom(src => src.GoodId))
+                .ForMember(dest => dest.GoodName, opt =>
+                    opt.MapFrom(src => src.Good.Name))
+                .ForMember(dest => dest.GoodImagePath, opt =>
+                    opt.MapFrom(src => src.Good.PhotoPath))
+                .ForMember(dest => dest.GoodSellingPrice, opt =>
+                    opt.MapFrom(src => src.Good.SellingPrice))
+                .ForMember(dest => dest.GoodDiscount, opt =>
+                    opt.MapFrom(src => src.Good.Discount))
+                .ForMember(dest => dest.OrderId, opt =>
+                    opt.MapFrom(src => src.OrderId))
+                .ForMember(dest => dest.Count, opt =>
+                    opt.MapFrom(src => src.Count));
+        }
+    }
+}
