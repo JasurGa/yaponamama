d1dfab9014900ab4f5a1a67daa2d7462119e5b75
telesium <telesium@protonmail.com>
2022-08-27 17:38:23 +0500

    Categories to good added

--- Atlas.Application/CQRS/Goods/Queries/GetGoodDetails/GetGoodDetailsQueryHandler.cs
+++ Atlas.Application/CQRS/Goods/Queries/GetGoodDetails/GetGoodDetailsQueryHandler.cs
@@ -6,6 +6,10 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Atlas.Application.Common.Exceptions;
 using Atlas.Domain;
+using Neo4j.Driver;
+using System.Collections.Generic;
+using Atlas.Application.CQRS.Categories.Queries.GetCategoryList;
+using Atlas.Application.Common.Helpers;
 
 namespace Atlas.Application.CQRS.Goods.Queries.GetGoodDetails
 {
@@ -13,6 +17,7 @@ namespace Atlas.Application.CQRS.Goods.Queries.GetGoodDetails
         GoodDetailsVm>
     {
         private readonly IMapper         _mapper;
+        private readonly IDriver         _driver;
         private readonly IAtlasDbContext _dbContext;
 
         public GetGoodDetailsQueryHandler(IMapper mapper, IAtlasDbContext dbContext) =>
@@ -29,7 +34,31 @@ namespace Atlas.Application.CQRS.Goods.Queries.GetGoodDetails
                 throw new NotFoundException(nameof(Good), request.Id);
             }
 
-            return _mapper.Map<Good, GoodDetailsVm>(good);
+            var categories = new List<CategoryLookupDto>();
+
+            var session = _driver.AsyncSession();
+            try
+            {
+                var cursor = await session.RunAsync("MATCH (c:Category{IsDeleted: false})<-[:BELONGS_TO]-(p:Good{Id: $Id}) OPTIONAL MATCH (c)<-[:BELONGS_TO]-(ch:Category{IsDeleted: false}) OPTIONAL MATCH (c)<-[:BELONGS_TO*]-(g:Good) RETURN {ImageUrl: c.ImageUrl, IsDeleted: c.IsDeleted, Id:c.Id, IsMainCategory: c.IsMainCategory, Name: c.Name, ChildCategoriesCount: COUNT(DISTINCT ch), GoodsCount: COUNT(DISTINCT g)} AS r", new
+                {
+                    Id = request.Id.ToString(),
+                });
+
+                categories = _mapper.Map<List<Category>, List<CategoryLookupDto>>(
+                    await cursor.ConvertDictManyAsync<Category>());
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+
+            return _mapper.Map<Good, GoodDetailsVm>(good, opt =>
+            {
+                opt.AfterMap((src, dst) =>
+                {
+                    dst.Categories = categories;
+                });
+            });
         }
     }
 }
