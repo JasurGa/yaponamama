dec3331666c6dee20ba8ab2e4bd81b7130e1d5a2
telesium <telesium@protonmail.com>
2022-09-13 17:56:45 +0500

    Supports search refactored

--- Atlas.Application/CQRS/Supports/Queries/FindSupportsPagedList/FindSupportsPagedListQueryValidator.cs
+++ Atlas.Application/CQRS/Supports/Queries/FindSupportsPagedList/FindSupportsPagedListQueryValidator.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using FluentValidation;
+
 namespace Atlas.Application.CQRS.Supports.Queries.FindSupportsPagedList
 {
-    public class FindSupportsPagedListQueryValidator
+    public class FindSupportsPagedListQueryValidator : AbstractValidator<FindSupportsPagedListQuery>
     {
         public FindSupportsPagedListQueryValidator()
         {
+            RuleFor(x => x.SearchQuery)
+                .NotNull();
+
+            RuleFor(x => x.PageSize)
+                .GreaterThan(0);
+
+            RuleFor(x => x.PageIndex)
+                .GreaterThanOrEqualTo(0);
         }
     }
 }
