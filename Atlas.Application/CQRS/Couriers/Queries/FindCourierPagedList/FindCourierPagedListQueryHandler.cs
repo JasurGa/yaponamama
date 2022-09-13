5c889b6fb40d1c32a07371347ac1c6e5a5d625a1
telesium <telesium@protonmail.com>
2022-09-13 17:51:59 +0500

    Couriers search refactored

--- Atlas.Application/CQRS/Couriers/Queries/FindCourierPagedList/FindCourierPagedListQueryHandler.cs
+++ Atlas.Application/CQRS/Couriers/Queries/FindCourierPagedList/FindCourierPagedListQueryHandler.cs
@@ -35,7 +35,7 @@ namespace Atlas.Application.CQRS.Couriers.Queries.FindCourierPagedList
                 couriers.Where(x => x.Vehicle.StoreId == request.FilterStoreId);
             }
 
-            couriers = couriers.OrderBy(x => EF.Functions.TrigramsWordSimilarityDistance($"{x.PhoneNumber} {x.User.Login} {x.User.FirstName} {x.User.LastName} {x.User.MiddleName}",
+            couriers = couriers.OrderBy(x => EF.Functions.TrigramsWordSimilarityDistance($"{x.PhoneNumber} {x.User.Login} {x.User.FirstName} {x.User.LastName} {x.User.MiddleName}".ToLower().Trim(),
                 request.SearchQuery));
 
             var couriersCount = await couriers.CountAsync(cancellationToken);
