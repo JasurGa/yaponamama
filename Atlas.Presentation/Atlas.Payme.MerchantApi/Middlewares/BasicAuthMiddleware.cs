8a36566b39e3bf43b00baecc4b55a74233fa55dd
telesium <telesium@protonmail.com>
2022-10-07 17:08:47 +0500

    Payme merchant api refactored v6

--- Atlas.Presentation/Atlas.Payme.MerchantApi/Middlewares/BasicAuthMiddleware.cs
+++ Atlas.Presentation/Atlas.Payme.MerchantApi/Middlewares/BasicAuthMiddleware.cs
@@ -36,7 +36,7 @@ namespace Atlas.Payme.MerchantApi.Middlewares
                 else
                 {
                     httpContext.Response.StatusCode = (int)HttpStatusCode.OK;
-                    var bytes = Encoding.UTF8.GetBytes("{\"id\":0,\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32605,\"message\":\"An Rpc error occurred while trying to invoke request.\",\"data\":\"\"}}");
+                    var bytes = Encoding.UTF8.GetBytes("{\"id\":0,\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32504,\"message\":\"An Rpc error occurred while trying to invoke request.\",\"data\":\"\"}}");
                     await httpContext.Response.Body.WriteAsync(bytes, 0, bytes.Length);
                     return;
                 }
@@ -44,7 +44,7 @@ namespace Atlas.Payme.MerchantApi.Middlewares
             else
             {
                 httpContext.Response.StatusCode = (int)HttpStatusCode.OK;
-                var bytes = Encoding.UTF8.GetBytes("{\"id\":0,\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32605,\"message\":\"An Rpc error occurred while trying to invoke request.\",\"data\":\"\"}}");
+                var bytes = Encoding.UTF8.GetBytes("{\"id\":0,\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32504,\"message\":\"An Rpc error occurred while trying to invoke request.\",\"data\":\"\"}}");
                 await httpContext.Response.Body.WriteAsync(bytes, 0, bytes.Length);
                 return;
             }
