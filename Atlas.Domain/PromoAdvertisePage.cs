0d150a17f7a4be24f4d60d56fc455b360d84d521
telesium <telesium@protonmail.com>
2022-11-10 13:54:10 +0500

    Feature: Promo database models added

---
+++ Atlas.Domain/PromoAdvertisePage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atlas.Domain
+{
+    public class PromoAdvertisePage
+    {
+        public Guid Id { get; set; }
+
+        public Guid PromoAdvertiseId { get; set; }
+
+        public PromoAdvertise PromoAdvertise { get; set; }
+
+        public string BadgeColor { get; set; }
+
+        public string BadgeTextRu { get; set; }
+
+        public string BadgeTextEn { get; set; }
+
+        public string BadgeTextUz { get; set; }
+
+        public string TitleColor { get; set; }
+
+        public string TitleRu { get; set; }
+
+        public string TitleEn { get; set; }
+
+        public string TitleUz { get; set; }
+
+        public string DescriptionColor { get; set; }
+
+        public string DescriptionRu { get; set; }
+
+        public string DescriptionEn { get; set; }
+
+        public string DescriptionUz { get; set; }
+
+        public string ButtonColor { get; set; }
+
+        public ICollection<PromoAdvertiseGood> PromoAdvertiseGoods { get; set; }
+    }
+}
+
---
+++ Atlas.Infrastructure/EntityTypeConfigurations/PromoAdvertiseConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Atlas.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Atlas.Persistence.EntityTypeConfigurations
+{
+    public class PromoAdvertiseConfiguration : IEntityTypeConfiguration<PromoAdvertise>
+    {
+        public void Configure(EntityTypeBuilder<PromoAdvertise> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Id).IsUnique();
+            builder.HasMany(pa => pa.PromoAdvertisePages)
+                .WithOne(pap => pap.PromoAdvertise);
+        }
+    }
+}
+
