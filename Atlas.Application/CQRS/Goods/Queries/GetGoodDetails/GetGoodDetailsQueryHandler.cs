293d01e6622b40563b991abc34e48a7a83b8010c
telesium <telesium@protonmail.com>
2023-01-06 14:20:36 +0500

    Fix: Get good by id now returning first IsMainCategory true v3

--- Atlas.Application/CQRS/Goods/Queries/GetGoodDetails/GetGoodDetailsQueryHandler.cs
+++ Atlas.Application/CQRS/Goods/Queries/GetGoodDetails/GetGoodDetailsQueryHandler.cs
@@ -47,7 +47,7 @@ namespace Atlas.Application.CQRS.Goods.Queries.GetGoodDetails
 
                 categories = _mapper.Map<List<Category>, List<CategoryLookupDto>>(
                     await cursor.ConvertDictManyAsync<Category>())
-                    .OrderByDescending(x => x.IsMainCategory).ToList();
+                    .OrderBy(x => x.IsMainCategory).ToList();
             }
             finally
             {
