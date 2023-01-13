a8f87d6cff10cbc40ba4ac9d665c5286501f0a1a
telesium <telesium@protonmail.com>
2023-01-13 16:14:43 +0500

    Feature: Good model in Favorite Good added

--- Atlas.Infrastructure/EntityTypeConfigurations/FavoriteGoodConfiguration.cs
+++ Atlas.Infrastructure/EntityTypeConfigurations/FavoriteGoodConfiguration.cs
@@ -11,6 +11,9 @@ namespace Atlas.Persistence.EntityTypeConfigurations
         {
             builder.HasKey(x => x.Id);
             builder.HasIndex(x => x.Id).IsUnique();
+
+            builder.HasOne(x => x.Good)
+                .WithMany();
         }
     }
 }
