ee3c8ac6207aad28b7cb1b6bfe0725a1bd55997b
telesium <telesium@protonmail.com>
2023-02-15 15:58:09 +0500

    Bugfix: migrate.sh file changed

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+export PATH="$PATH:/root/.dotnet"
+export PATH="$PATH:/root/.dotnet/tools"
+
+export PATH=$PATH:$HOME/.dotnet/tools
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$PATH:$DOTNET_ROOT
+
 echo "[*] Applying migrations..."
 
 if [ "$#" -ne 1 ]; then
