6d3f06032bcdaefe4220031d1cc3c4de8fd295c8
telesium <telesium@protonmail.com>
2023-02-20 15:09:23 +0500

    Promo objects added v2

--- Atlas.Application/CQRS/Promos/Queries/GetPromoList/PromoLookupDto.cs
+++ Atlas.Application/CQRS/Promos/Queries/GetPromoList/PromoLookupDto.cs
@@ -13,6 +13,14 @@ namespace Atlas.Application.CQRS.Promos.Queries.GetPromoList
 
         public string Name { get; set; }
 
+        public int DiscountPrice { get; set; }
+
+        public int DiscountPercent { get; set; }
+
+        public bool ForAllGoods { get; set; }
+
+        public bool FreeDelivery { get; set; }
+
         public DateTime ExpiresAt { get; set; }
 
         public void Mapping(Profile profile)
@@ -24,6 +32,14 @@ namespace Atlas.Application.CQRS.Promos.Queries.GetPromoList
                     opt.MapFrom(src => src.ClientId))
                 .ForMember(dst => dst.Name, opt =>
                     opt.MapFrom(src => src.Name))
+                .ForMember(dst => dst.DiscountPrice, opt =>
+                    opt.MapFrom(src => src.DiscountPrice))
+                .ForMember(dst => dst.DiscountPercent, opt =>
+                    opt.MapFrom(src => src.DiscountPercent))
+                .ForMember(dst => dst.ForAllGoods, opt =>
+                    opt.MapFrom(src => src.ForAllGoods))
+                .ForMember(dst => dst.FreeDelivery, opt =>
+                    opt.MapFrom(src => src.FreeDelivery))
                 .ForMember(dst => dst.ExpiresAt, opt =>
                     opt.MapFrom(src => src.ExpiresAt));
         }
