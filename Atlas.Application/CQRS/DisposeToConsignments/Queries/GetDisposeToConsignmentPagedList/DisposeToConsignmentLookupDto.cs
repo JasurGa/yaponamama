07c1cbbe6f74265b8f7578ea08087e6be3301920
Deastortion <deastortion@gmail.com>
2023-02-22 10:17:20 +0500

    Added getting paged list of disposed consignments

---
+++ Atlas.Application/CQRS/DisposeToConsignments/Queries/GetDisposeToConsignmentPagedList/DisposeToConsignmentLookupDto.cs
@@ -0,0 +1,39 @@
+﻿using Atlas.Application.Common.Mappings;
+using Atlas.Domain;
+using AutoMapper;
+using System;
+
+namespace Atlas.Application.CQRS.DisposeToConsignments.Queries.GetDisposeToConsignmentPagedList
+{
+    public class DisposeToConsignmentLookupDto : IMapWith<DisposeToConsignment>
+    {
+        public Guid Id { get; set; }
+
+        public Guid ConsignmentId { get; set; }
+
+        public DisposeToConsignmentGoodLookupDto Good { get; set; }
+
+        public int Count { get; set; }
+
+        public string Comment { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<DisposeToConsignment, DisposeToConsignmentLookupDto>()
+                .ForMember(dst => dst.Id, opt =>
+                    opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.ConsignmentId, opt =>
+                    opt.MapFrom(src => src.ConsignmentId))
+                .ForMember(dst => dst.Good, opt =>
+                    opt.MapFrom(src => src.Consignment.StoreToGood.Good))
+                .ForMember(dst => dst.Count, opt =>
+                    opt.MapFrom(src => src.Count))
+                .ForMember(dst => dst.Comment, opt =>
+                    opt.MapFrom(src => src.Comment))
+                .ForMember(dst => dst.CreatedAt, opt =>
+                    opt.MapFrom(src => src.CreatedAt));
+        }
+    }
+}
---
+++ Atlas.Application/CQRS/DisposeToConsignments/Queries/GetDisposeToConsignmentPagedList/GetDisposeToConsignmentPagedListQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Atlas.Application.CQRS.DisposeToConsignments.Queries.GetDisposeToConsignmentPagedList
+{
+    public class GetDisposeToConsignmentPagedListQueryValidator : AbstractValidator<GetDisposeToConsignmentPagedListQuery>
+    {
+        public GetDisposeToConsignmentPagedListQueryValidator()
+        {
+            RuleFor(x => x.PageSize)
+                .NotEmpty();
+        }
+    }
+}
