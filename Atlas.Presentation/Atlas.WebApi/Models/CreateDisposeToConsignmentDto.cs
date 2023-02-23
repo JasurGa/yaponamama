9202c864931991e6981eca1ef747f039846f77b2
Deastortion <deastortion@gmail.com>
2023-02-23 15:25:58 +0500

    Bugfix: Disposal records creating fixed v2

--- Atlas.Presentation/Atlas.WebApi/Models/CreateDisposeToConsignmentDto.cs
+++ Atlas.Presentation/Atlas.WebApi/Models/CreateDisposeToConsignmentDto.cs
@@ -1,5 +1,6 @@
 ﻿using Atlas.Application.Common.Mappings;
 using Atlas.Application.CQRS.DisposeToConsignments.Commands.CreateDisposeToConsignment;
+using AutoMapper;
 using System;
 
 namespace Atlas.WebApi.Models
@@ -11,5 +12,16 @@ namespace Atlas.WebApi.Models
         public int Count { get; set; }
 
         public string Comment { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateDisposeToConsignmentDto, CreateDisposeToConsignmentCommand>()
+                .ForMember(dst => dst.ConsignmentId, opt =>
+                    opt.MapFrom(src => src.ConsignmentId))
+                .ForMember(dst => dst.Count, opt =>
+                    opt.MapFrom(src => src.Count))
+                .ForMember(dst => dst.Comment, opt =>
+                    opt.MapFrom(src => src.Comment));
+        }
     }
 }
