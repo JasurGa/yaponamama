f87d7a6e9885f4047fb5f650ae8162667607f548
Deastortion <deastortion@gmail.com>
2023-03-08 02:47:50 +0500

    Feature: user login update with verification codes added

---
+++ Atlas.Presentation/Atlas.WebApi/Models/UpdateUserLoginDto.cs
@@ -0,0 +1,23 @@
+﻿using Atlas.Application.Common.Mappings;
+using Atlas.Application.CQRS.Users.Commands.UpdateUserLogin;
+using AutoMapper;
+using System;
+
+namespace Atlas.WebApi.Models
+{
+    public class UpdateUserLoginDto : IMapWith<UpdateUserLoginCommand>
+    {
+        public Guid Id { get; set; }
+
+        public string Login { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdateUserLoginDto, UpdateUserLoginCommand>()
+                .ForMember(x => x.Id, opt =>
+                    opt.MapFrom(x => x.Id))
+                .ForMember(x => x.Login, opt =>
+                    opt.MapFrom(x => x.Login));
+        }
+    }
+}
