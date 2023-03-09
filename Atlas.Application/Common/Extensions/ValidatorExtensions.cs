f232c64cb55ed8bf15baf4150ca911e708dced62
Deastortion <deastortion@gmail.com>
2023-03-10 02:30:24 +0500

    Bugfix: phone number validator country code requirement fixed

--- Atlas.Application/Common/Extensions/ValidatorExtensions.cs
+++ Atlas.Application/Common/Extensions/ValidatorExtensions.cs
@@ -7,7 +7,7 @@ namespace Atlas.Application.Common.Extensions
         public static IRuleBuilderOptions<T, string> PhoneNumber<T>(this IRuleBuilder<T, string> ruleBuilder)
         {
             return ruleBuilder
-                .Must(x => !x.StartsWith("+998"))
+                .Must(x => x.StartsWith("+998"))
                     .WithMessage("Phone number must start with +998")
                 .Length(13)
                     .WithMessage("Phone number digits count must be 13");
