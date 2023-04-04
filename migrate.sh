bda3f04374b981efeea79c6d2ac6615a9cb7f115
Dominik Galiev <92969613+telesium@users.noreply.github.com>
2023-04-04 15:07:12 +0200

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -8,7 +8,7 @@
 
 export PATH="$PATH:$HOME/.dotnet/tools"
 export DOTNET_ROOT=/snap/dotnet-sdk/current
-export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
+export MSBuildSDKsPath=/usr/share/dotnet/sdk/$(dotnet --version)/Sdks/
 export PATH="${PATH}:${DOTNET_ROOT}"
 export PATH="$PATH:$HOME/.dotnet/tools"
 
