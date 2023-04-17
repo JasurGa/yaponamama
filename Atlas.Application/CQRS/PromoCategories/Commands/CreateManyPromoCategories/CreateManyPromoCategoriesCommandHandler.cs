ce1a1dca710805c727cbddb468eadddb37387ecb
Deastortion <deastortion@gmail.com>
2023-04-17 23:17:11 +0500

    Feature: several promo categories endpoint added

---
+++ Atlas.Application/CQRS/PromoCategories/Commands/CreateManyPromoCategories/CreateManyPromoCategoriesCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Atlas.Application.Interfaces;
+using Atlas.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Atlas.Application.CQRS.PromoCategories.Commands.CreateManyPromoCategories
+{
+    public class CreateManyPromoCategoriesCommandHandler : IRequestHandler<CreateManyPromoCategoriesCommand, List<Guid>>
+    {
+        private readonly IAtlasDbContext _dbContext;
+        public CreateManyPromoCategoriesCommandHandler(IAtlasDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<List<Guid>> Handle(CreateManyPromoCategoriesCommand request, CancellationToken cancellationToken)
+        {
+            var promoCategoryIds = new List<Guid>();
+
+            foreach (var requestPromoCategory in request.PromoCategories)
+            {
+                var promoCategory = new PromoCategory
+                {
+                    Id       = Guid.NewGuid(),
+                    NameEn   = requestPromoCategory.NameEn,
+                    NameRu   = requestPromoCategory.NameRu,
+                    NameUz   = requestPromoCategory.NameUz,
+                    ImageUrl = requestPromoCategory.ImageUrl,
+                };
+
+                await _dbContext.PromoCategories.AddAsync(promoCategory);
+
+                promoCategoryIds.Add(promoCategory.Id);
+            }
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return promoCategoryIds;
+        }
+    }
+}
---
+++ Atlas.Application/CQRS/PromoCategories/Commands/CreateManyPromoCategories/CreateManyPromoCategoriesCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Atlas.Application.CQRS.PromoCategories.Commands.CreateManyPromoCategories
+{
+    public class CreateManyPromoCategoriesCommandValidator : AbstractValidator<CreateManyPromoCategoriesCommand>
+    {
+        public CreateManyPromoCategoriesCommandValidator()
+        {
+
+        }
+    }
+}
