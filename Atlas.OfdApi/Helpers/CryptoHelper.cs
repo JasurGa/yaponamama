43e58e96d7b2940387acee5b4bee21f8f01cfbd8
telesium <telesium@protonmail.com>
2023-04-19 15:30:38 +0200

    Changes added

--- Atlas.OfdApi/Helpers/CryptoHelper.cs
+++ Atlas.OfdApi/Helpers/CryptoHelper.cs
@@ -1,13 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography.Pkcs;
+using System.IO;
+using Org.BouncyCastle.Cms;
+using Org.BouncyCastle.X509;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto.Operators;
 
 namespace Atlas.OfdApi.Helpers
 {
     public static class CryptoHelper
     {
+        public static byte[] EncodeData(byte[] data, X509Certificate certificate)
+        {
+            var generator = new CmsSignedDataGenerator();
+            generator.AddCertificate(certificate);
+
+            var encoded = generator.Generate(new CmsProcessableByteArray(data), false);
+            return encoded.GetEncoded();
+        }
+
+        public static Pkcs10CertificationRequest ParseCsrFile(string file)
+        {
+            var content = File.ReadAllText(file)
+                .Replace("-----BEGIN CERTIFICATE REQUEST-----", "")
+                .Replace("-----END CERTIFICATE REQUEST-----", "")
+                .Replace("\n", "");
+
+            return ParseCsrPemContent(content);
+        }
+
+        public static Pkcs10CertificationRequest ParseCsrPemContent(string content)
+        {
+            return new Pkcs10CertificationRequest(Convert.FromBase64String(content));
+        }
+
+        public static X509Certificate GetCertificate(Pkcs10CertificationRequest csr, AsymmetricKeyParameter privateKey)
+        {
+            var notBefore = DateTime.UtcNow.AddDays(-1);
+            var notAfter  = DateTime.UtcNow.AddDays(7);
+            var serialNum = DateTime.UtcNow.Ticks.ToString();
+
+            var certGen = new X509V3CertificateGenerator();
+            certGen.SetSerialNumber(new BigInteger(serialNum));
+            certGen.SetIssuerDN(csr.GetCertificationRequestInfo().Subject);
+            certGen.SetSubjectDN(csr.GetCertificationRequestInfo().Subject);
+            certGen.SetNotBefore(notBefore);
+            certGen.SetNotAfter(notAfter);
+            certGen.SetPublicKey(csr.GetPublicKey());
+
+            return certGen.Generate(new Asn1SignatureFactory("SHA256WithRSAEncryption", privateKey));
+        }
+
+        public static AsymmetricKeyParameter ParseKeyFile(string file)
+        {
+            var fileStream = File.OpenText(file);
+            var pemReader  = new PemReader(fileStream);
+            return (AsymmetricKeyParameter)pemReader.ReadObject();
+        }
     }
 }
--- Atlas.Test/Program.cs
+++ Atlas.Test/Program.cs
@@ -1,73 +1,21 @@
-﻿using Org.BouncyCastle.Asn1;
-using Org.BouncyCastle.Asn1.Ocsp;
-using Org.BouncyCastle.Asn1.Oiw;
-using Org.BouncyCastle.Asn1.Pkcs;
-using Org.BouncyCastle.Asn1.X509;
-using Org.BouncyCastle.Cms;
-using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Encodings;
-using Org.BouncyCastle.Crypto.Operators;
-using Org.BouncyCastle.Math;
-using Org.BouncyCastle.OpenSsl;
-using Org.BouncyCastle.Operators;
-using Org.BouncyCastle.Pkcs;
-using Org.BouncyCastle.X509;
+﻿using Atlas.OfdApi.Helpers;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Text;
 
 namespace Atlas.Test
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         { 
-            var key         = ParseKeyFile("C:\\Users\\User\\Desktop\\OFD\\user1.key");
-            var request     = ParseCsrFile("C:\\Users\\User\\Desktop\\OFD\\user1.csr");
-            var certificate = GetCertificate(request, key);
+            var privateKey  = CryptoHelper.ParseKeyFile("C:\\Users\\User\\Desktop\\OFD\\user1.key");
+            var certRequest = CryptoHelper.ParseCsrFile("C:\\Users\\User\\Desktop\\OFD\\user1.csr");
+            var certificate = CryptoHelper.GetCertificate(certRequest, privateKey);
 
-            var data = "Hello, world!";
-
-            var gen = new CmsSignedDataGenerator();
-            gen.AddCertificate(certificate);
-            var encoded = gen.Generate(new CmsProcessableByteArray(Encoding.UTF8.GetBytes(data)), false);
-            Console.WriteLine(Encoding.UTF8.GetString(encoded.GetEncoded()));
-        }
-
-        public static Pkcs10CertificationRequest ParseCsrFile(string file)
-        {
-            var content = File.ReadAllText(file);
-            content = content.Replace("-----BEGIN CERTIFICATE REQUEST-----", "");
-            content = content.Replace("-----END CERTIFICATE REQUEST-----", "");
-            content = content.Replace("\n", "");
-
-            return new Pkcs10CertificationRequest(Convert.FromBase64String(content));
-        }
-
-        public static X509Certificate GetCertificate(Pkcs10CertificationRequest csr, AsymmetricKeyParameter privateKey)
-        {
-            var notBefore = DateTime.UtcNow;
-            var notAfter  = DateTime.UtcNow.AddDays(7);
-
-            var certGen = new X509V3CertificateGenerator();
-            certGen.SetSerialNumber(new BigInteger(DateTime.UtcNow.Ticks.ToString()));
-            certGen.SetIssuerDN(csr.GetCertificationRequestInfo().Subject);
-            certGen.SetSubjectDN(csr.GetCertificationRequestInfo().Subject);
-            certGen.SetNotBefore(notBefore);
-            certGen.SetNotAfter(notAfter);
-            certGen.SetPublicKey(csr.GetPublicKey());
+            var message = Encoding.UTF8.GetBytes("Hello, world!");
+            var encoded = Encoding.UTF8.GetString(CryptoHelper.EncodeData(message, certificate));
             
-            return certGen.Generate(new Asn1SignatureFactory("SHA256WithRSAEncryption", privateKey));
-        }
-
-        public static AsymmetricKeyParameter ParseKeyFile(string file)
-        {
-            var fileStream = File.OpenText(file);
-            var pemReader = new PemReader(fileStream);
-            var result = pemReader.ReadObject();
-
-            return (AsymmetricKeyParameter)result;
+            Console.WriteLine(encoded);
         }
     }
 }
