1aa8526d56bdebea3aab6b8009b68761ecbf12bd
telesium <telesium@protonmail.com>
2023-04-19 14:33:37 +0200

    OFD api added

---
+++ Atlas.OfdApi/Models/TaxiInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlas.OfdApi.Models
+{
+    public class TaxiInfo
+    {
+        public string TIN { get; set; }
+
+        public string PINFL { get; set; }
+
+        public string CarNumber { get; set; }
+    }
+}
